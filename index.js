6f1c54df0aff0ef40057d214c6f176b215a5dd00
lucianojunior3 <>
2021-05-20 18:27:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,184 @@
+var parar = 0;
+while (parar == 0){
+console.log("")
+console.log("bem vindo a make's girl")
+console.log("")
+console.log("Produtos:")
+console.log("Batom")
+console.log("Base")
+console.log("Rimel")
+console.log("")
+var expr = prompt("Escolha um produto")
+
+if (expr == 'Batom'){
+
+console.log("")
+console.log("perfeito!")
+console.log("vai ser pago no dinheiro ou cartão?")
+var forma = prompt("escolha uma forma de pagamento")
+
+switch (forma){
+
+  case 'dinheiro' :
+
+console.log("")
+console.log("perfeito o motoboy ira até sua residencia basta pagar para ele")
+console.log("")
+for(var expf = prompt("Digite seu endereço:"); expf <= 3; expf++){console.log(expr)}
+console.log("")
+console.log("perfeito! o entregador esta a caminho")
+console.log("perfeito temos 230 entregas feitas")
+var ete = 230;
+ console.log("")
+  console.log("com a sua temos")
+   console.log(ete + 1)
+    console.log("")
+     console.log("obrigado por confiar no nosso pessoal")
+  break
+
+  case 'cartão' :
+console.log("")
+console.log("perfeito preciso que confirme uma senha")
+console.log("sua senha é 846")
+var expz = prompt("confime a senha")
+do {
+  console.log("obrigado pela confirmação")
+} while(expz === 846)
+
+console.log("")
+console.log("Digite as informações")
+    console.log("")
+    var t  = prompt("Débito ou Crédito?")
+    console.log("")
+    var t  =  prompt("Insira o numero do cartão:")
+    console.log("")
+    var t  = prompt("Insira o CVV:")
+    console.log("")
+    var t  = prompt("Insira a senha:")
+console.log("perfeito temos 230 entregas feitas")
+var ete = 230;
+ console.log("")
+  console.log("com a sua temos")
+   console.log(ete + 1)
+    console.log("")
+     console.log("obrigado por confiar no nosso pessoal")
+  break
+}
+
+break}
+
+else if (expr == 'Base'){
+console.log("")
+console.log("perfeito!")
+console.log("vai ser pago no dinheiro ou cartão?")
+var forma = prompt("escolha uma forma de pagamento")
+
+switch (forma){
+
+  case 'dinheiro' :
+
+console.log("")
+console.log("perfeito o motoboy ira até sua residencia basta pagar para ele")
+console.log("")
+for(var expf = prompt("Digite seu endereço:"); expf <= 3; expf++){console.log(expr)}
+console.log("")
+console.log("perfeito! o entregador esta a caminho")
+console.log("perfeito temos 230 entregas feitas")
+var ete = 230;
+ console.log("")
+  console.log("com a sua temos")
+   console.log(ete + 1)
+    console.log("")
+     console.log("obrigado por confiar no nosso pessoal")
+  break
+
+  case 'cartão' :
+console.log("")
+console.log("perfeito preciso que confirme uma senha")
+console.log("sua senha é 846")
+var expz = prompt("confime a senha")
+do {
+  console.log("obrigado pela confirmação")
+} while(expz === 846)
+
+console.log("")
+console.log("Digite as informações")
+    console.log("")
+    var t  = prompt("Débito ou Crédito?")
+    console.log("")
+    var t  =  prompt("Insira o numero do cartão:")
+    console.log("")
+    var t  = prompt("Insira o CVV:")
+    console.log("")
+    var t  = prompt("Insira a senha:")
+console.log("perfeito temos 230 entregas feitas")
+var ete = 230;
+ console.log("")
+  console.log("com a sua temos")
+   console.log(ete + 1)
+    console.log("")
+     console.log("obrigado por confiar no nosso pessoal")
+  break
+}
+break}
+
+else if (expr == 'Rimel'){
+console.log("")
+console.log("perfeito!")
+console.log("vai ser pago no dinheiro ou cartão?")
+var forma = prompt("escolha uma forma de pagamento")
+
+switch (forma){
+
+  case 'dinheiro' :
+
+console.log("")
+console.log("perfeito o motoboy ira até sua residencia basta pagar para ele")
+console.log("")
+for(var expf = prompt("Digite seu endereço:"); expf <= 3; expf++){console.log(expr)}
+console.log("")
+console.log("perfeito! o entregador esta a caminho")
+console.log("perfeito temos 230 entregas feitas")
+var ete = 230;
+ console.log("")
+  console.log("com a sua temos")
+   console.log(ete + 1)
+    console.log("")
+     console.log("obrigado por confiar no nosso pessoal")
+  break
+
+  case 'cartão' :
+console.log("")
+console.log("perfeito preciso que confirme uma senha")
+console.log("sua senha é 846")
+var expz = prompt("confime a senha")
+do {
+  console.log("obrigado pela confirmação")
+} while(expz === 846)
+
+console.log("")
+console.log("Digite as informações")
+    console.log("")
+    var t  = prompt("Débito ou Crédito?")
+    console.log("")
+    var t  =  prompt("Insira o numero do cartão:")
+    console.log("")
+    var t  = prompt("Insira o CVV:")
+    console.log("")
+    var t  = prompt("Insira a senha:")
+console.log("perfeito temos 230 entregas feitas")
+var ete = 230;
+ console.log("")
+  console.log("com a sua temos")
+   console.log(ete + 1)
+    console.log("")
+     console.log("obrigado por confiar no nosso pessoal")
+  break
+}
+break}
+
+else if (expr != 'Batom' && expr != 'Rimel' && expr != 'Base'){
+
+}
+
+}
